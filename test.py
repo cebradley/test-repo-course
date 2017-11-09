f023cc4785c792c005d01ff480a791a27a296819
Conor Bradley <c.e.bradley@tudelft.nl>
2017-11-09 12:00:57 +0100

    Testing circle files V1

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print('Hello World')
+import numpy as np
+def area():
+	r = float(input('What is the radius of the circle?'))
+	area = np.pi*r**2
+	print('The area of a circle with radius ' + str(r) + ' is: ' + str(round(area,2))) 
+def circumference():
+	r = float(input('What is the radius of the circle?'))
+	circumference = 2*np.pi*r
+	print('The circumference of a circle with radius ' + str(r) + ' is: ' + str(round(circumference,2))) 
 
+area() 
+circumference()
